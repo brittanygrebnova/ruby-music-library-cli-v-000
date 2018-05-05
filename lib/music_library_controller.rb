09a8a3c3678c2f9c668f1eb1213e7447fc293bfa
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-05-05 02:48:03 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,7 +73,7 @@ class MusicLibraryController
     end
   end
   
-def play_song
+  def play_song
     puts "Which song number would you like to play?"
     user = gets.strip
     if user.to_i.between?(1, Song.all.length)
